cde08ad34bb66e5c7e7ca074cb125139263ae9d3
Liuyu648 <1104083800@qq.com>
2023-03-03 19:38:07 +0800

    first.c文件首次提交“
    :wq

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	printf("test output msg 01\n");
+	printf("test output msg 01\n");
+	printf("test output msg 01\n");
+	printf("test output msg 01\n");
+	printf("test output msg 01\n");
+	return 0;
+}
